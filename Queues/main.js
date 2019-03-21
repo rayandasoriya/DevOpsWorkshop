413d4e3cb58ab2d811d76bbe088b5a06e9ce9f98
Rayan Dasoriya <dasoriyarayan@gmail.com>
2019-03-20 20:21:16 -0400

    Added queues

---
+++ Queues/main.js
@@ -0,0 +1,107 @@
+var redis = require('redis')
+var multer  = require('multer')
+var express = require('express')
+var fs      = require('fs')
+var app = express()
+// REDIS
+var client = redis.createClient(6379, '127.0.0.1', {})
+
+///////////// WEB ROUTES
+
+// Add hook to make it easier to get all visited URLS.
+
+app.use(function(req, res, next) 
+{
+  console.log(req.method, req.url);
+  client.lpush('mylist',req.url);
+	// ... INSERT HERE.
+	next(); // Passing the request to the next handler in the stack.
+});
+
+app.get('/recent',function(req,res){
+  //client.get("myilist", function(err,value){ res.send(value)});
+  client.lrange('mylist',0,-1, (err,value) =>{
+    value.forEach(h =>{
+      res.write(h+'<br/>')
+    })
+    res.end();
+  })
+  
+	//res.send();
+})
+
+app.get('/',function(req,res){
+  res.send('Hello World');
+});
+
+app.get('/test', function(req, res) {
+		res.writeHead(200, {'content-type':'text/html'});
+		res.write("<h3>test</h3>");
+   	res.end();
+})
+
+app.get('/set',function(req,res){
+  res.writeHead(200, {'content-type':'text/html'});
+  client.set("key", "value");
+  res.write("This message will self-destruct in 10 seconds");
+  client.expire("key",10);
+  res.end();
+});
+
+
+app.get('/get',function(req,res){
+  client.get("key", function(err,value){ res.send(value)});
+});
+
+
+function get_line(filename, line_no, callback) {
+    var data = fs.readFileSync(filename, 'utf8');
+    var lines = data.split("\n");
+
+    if(+line_no > lines.length){
+      throw new Error('File end reached without finding line');
+    }
+
+    callback(null, lines[+line_no]);
+}
+
+// app.post('/upload',[ multer({ dest: './uploads/'}), function(req, res){
+//    console.log(req.body) // form fields
+//    console.log(req.files) // form files
+
+//    if( req.files.image )
+//    {
+// 	   fs.readFile( req.files.image.path, function (err, data) {
+// 	  		if (err) throw err;
+// 	  		var img = new Buffer(data).toString('base64');
+// 			console.log(img);
+			  
+// 			client.lpush('cats', img, function(err)
+// 			{
+
+// 				res.status(204).end()
+// 			});
+// 		});
+// 	}
+// }]);
+
+// app.get('/meow', function(req, res) {
+// 	{
+// 		res.writeHead(200, {'content-type':'text/html'});
+// 		console.log(err || imagedata)
+// 		res.write("<h1>\n<img src='data:my_pic.jpg;base64,"+imagedata+"'/>");
+// 		res.end();
+// 		//items.forEach(function (imagedata) 
+// 		//{
+// 		//});
+// 	}
+// })
+
+// HTTP SERVER
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+})
